bfa3a3ef502789fbe021dc00c5a4840d4847ff46
Dylan Gorman <90197475+dylangorman@users.noreply.github.com>
2021-11-20 23:08:51 +0000

    objects commit

--- objects.js
+++ objects.js
@@ -179,34 +179,39 @@ console.log(pet.drink())
 const coffeeShop = {
     branch: "Allerton Rd, Liverpool",
     drinks: [
-        "Cortado", 2.99, //[0],[1]
-        "Flat White", 2.75, // [2], [3]
-        "Americano", 2.50, // [4], [5]
-        "Filter Coffee", 1.50, // [6], [7]
-        "Tea", 1.50, //[8], [9]
-        "Sparkling Water", 1.50, // [10], [11]
-        "Still Water", 1, // [12]. [13]
-        "Apple Juice", 1, // [14], [15]
+        "Cortado", 
+        "Flat White",
+        "Americano", 
+        "Filter Coffee",
+        "Tea",  
     ],
+    drinksPrices: {
+        Cortado: 2.99,
+        FlatWhite: 2.75, 
+        Americano: 2.50,
+        FilterCoffee: 1.50, 
+        Tea: 1.50, 
+    },
     food: [
-        "2 slices Plain Toast(with butter)",1.50, // [0], [1]
-        "Eggs on Toast", 2.50, //[2], [3]
-        "Eggs and Beans on Toast", 3.50, // [4], [5]
-        "Vegan Sausage Roll", 2.99, // [6], [7]
-        "Hummus and Bread", 3.75, // [8], [9]
+        "2 slices Plain Toast(with butter)",
+        "Eggs on Toast",  
+        "Eggs and Beans on Toast",
+        "Vegan Sausage Roll",
+        "Hummus and Bread",
     ],
-drinksOrdered(){
-    this.drinks;
-    return `Your drinks order is ${this.drinks[6]}:£${this.drinks[7]} and ${this.drinks[8]}:£${this.drinks[9]} and a ${this.drinks[2]}:£${this.drinks[3]}.`
+    foodPrices:{
+        slicesOfToast :1.50, 
+        eggsOnToast: 2.50, 
+        eggsAndBeansOnToast: 3.50,
+        veganSausageRoll: 2.99, 
+        hummusAndBread: 3.75,
     },
-foodOrdered(){
-    this.food;
-    return `Your food order is ${this.food[0]}:£${this.food[1]} and ${this.food[8]}:£${this.food[9]}.`
-    },
-total(){ 
-    return `Your total order comes to £ ${this.drinks[7]+ this.drinks[9]+ this.drinks[3]+ this.food[1]+ this.food[9]}`
-},
+order ( food,drink, price1,price2){
+    return ` You ordered ${food} and ${drink} and your total is £${price1 + price2} ` 
 }
-console.log(coffeeShop.drinksOrdered())
-console.log(coffeeShop.foodOrdered())
-console.log(coffeeShop.total())
+}
+let food = coffeeShop.food
+let drinks = coffeeShop.drinks
+let foodPrices = coffeeShop.foodPrices
+let drinksPrices = coffeeShop.drinksPrices
+console.log(coffeeShop.order(drinks[1],food[2], drinksPrices.FlatWhite,foodPrices.eggsAndBeansOnToast))
